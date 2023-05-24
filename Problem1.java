1b7e588d45b4683de3055646a1e9bec1ccc4a326
KimHJ-02 <131998142+KimHJ-02@users.noreply.github.com>
2023-05-24 16:43:18 +0900

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
 public class week13_1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        // 필요한 자료형을 넣어주세요
-        ArrayList< __________ > list = new ArrayList< __________ >();
-        System.out.print("Enter five integers and five doubles: ");
+		// 필요한 자료형을 넣어주세요
+		ArrayList<Double> list = new ArrayList<Double>();
+		System.out.print("Enter five integers and five doubles: ");
 
-        /*
-            list 입력 코드
-         */
+		for (int i = 0; i < 10; i++) {
+			double in = input.nextDouble();
+			list.add(in);
+		}
 
-        sort(list);
+		list = sort(list);
 
-        /*
-            list 출력 코드
-         */
-    }
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) % 1 == 0)
+				System.out.printf("%.0f ",list.get(i));
+			else if (list.get(i) % 1 != 0)
+				System.out.print(list.get(i) + " ");
+		}
+	}
 
-    public static void sort( __________ ) {
+	public static ArrayList<Double> sort(ArrayList<Double> arr) {
+		Collections.sort(arr);
 
-         /*
-            list 정렬 코드
-         */
-
-    }
+		return arr;
+	}
 }
